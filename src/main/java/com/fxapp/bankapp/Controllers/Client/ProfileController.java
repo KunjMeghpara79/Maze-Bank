cd93e4165592e37c14292ab0e25e93b590d5488d
Kunj Meghpara <163090570+KunjMeghpara79@users.noreply.github.com>
2025-06-10 09:56:06 +0530

    Redesign Profile Page - Personal Information Only

--- src/main/java/com/fxapp/bankapp/Controllers/Client/ProfileController.java
+++ src/main/java/com/fxapp/bankapp/Controllers/Client/ProfileController.java
@@ -8,55 +8,57 @@ import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class ProfileController implements Initializable {
 
+    // Header section labels
     public Label first_name_lbl;
     public Label last_name_lbl;
     public Label payee_address_lbl;
     public Label date_created_lbl;
-    public Label checking_account_lbl;
-    public Label savings_account_lbl;
-    public Label checking_balance_lbl;
-    public Label savings_balance_lbl;
+    
+    // Detail section labels
+    public Label first_name_detail;
+    public Label last_name_detail;
+    public Label payee_address_detail;
+    public Label date_created_detail;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        bindClientData();
+        bindClientPersonalData();
     }
 
-    private void bindClientData() {
-        // Bind client personal information
+    private void bindClientPersonalData() {
+        // Get client properties
         StringProperty firstName = Model.getInstance().getClient().getFirstNameProperty();
-        this.first_name_lbl.textProperty().bind(firstName);
-
         StringProperty lastName = Model.getInstance().getClient().getLastNameProperty();
-        this.last_name_lbl.textProperty().bind(lastName);
-
         StringProperty payeeAddress = Model.getInstance().getClient().getPayeeAddressProperty();
-        this.payee_address_lbl.textProperty().bind(payeeAddress);
-
         ObjectProperty<LocalDate> dateCreated = Model.getInstance().getClient().getDateCreatedProperty();
-        this.date_created_lbl.textProperty().bind(dateCreated.asString());
 
-        // Bind account information
-        if (Model.getInstance().getClient().getCheckingAccountProperty().get() != null) {
-            StringProperty checkingAccountNumber = 
-                Model.getInstance().getClient().getCheckingAccountProperty().get().getAccountNumberProperty();
-            this.checking_account_lbl.textProperty().bind(checkingAccountNumber);
-
-            this.checking_balance_lbl.textProperty().bind(
-                Model.getInstance().getClient().getCheckingAccountProperty().get().getBalanceProperty().asString());
+        // Bind header section
+        this.first_name_lbl.textProperty().bind(firstName);
+        this.last_name_lbl.textProperty().bind(lastName);
+        this.payee_address_lbl.textProperty().bind(payeeAddress);
+        
+        // Format and bind date for header
+        if (dateCreated.get() != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+            String formattedDate = dateCreated.get().format(formatter);
+            this.date_created_lbl.setText(formattedDate);
         }
 
-        if (Model.getInstance().getClient().getSavingsAccountProperty().get() != null) {
-            StringProperty savingsAccountNumber = 
-                Model.getInstance().getClient().getSavingsAccountProperty().get().getAccountNumberProperty();
-            this.savings_account_lbl.textProperty().bind(savingsAccountNumber);
-
-            this.savings_balance_lbl.textProperty().bind(
-                Model.getInstance().getClient().getSavingsAccountProperty().get().getBalanceProperty().asString());
+        // Bind detail section
+        this.first_name_detail.textProperty().bind(firstName);
+        this.last_name_detail.textProperty().bind(lastName);
+        this.payee_address_detail.textProperty().bind(payeeAddress);
+        
+        // Format and bind date for details section
+        if (dateCreated.get() != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+            String formattedDate = dateCreated.get().format(formatter);
+            this.date_created_detail.setText(formattedDate);
         }
     }
 }
